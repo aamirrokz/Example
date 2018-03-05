e9525ebb09b3b98e5a87d09faca4224e47644aa0
aamirrokz <ac4600@gmail.com>
2018-03-05 12:05:58 +0530

    added file factorial

---
+++ factorial.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var solution=1;
+var num=12;
+
+for (var x=1;x<=num;x++){
+   solution*=x;
+   console.log(solution);
+}
+console.log(solution);
